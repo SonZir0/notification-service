f20921e936d002e19782cd508d578ffc2999267c
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2026-02-22 04:24:31 +0400

    Add Kafka and MailPit

---
+++ src/main/java/org/example/notification_service/service/KafkaNotificationsConsumer.java
@@ -0,0 +1,23 @@
+package org.example.notification_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class KafkaNotificationsConsumer {
+    EmailService emailService;
+
+    @Autowired
+    KafkaNotificationsConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @KafkaListener(groupId = "testGroup", topics = "emailNotifications")
+    public void listenToTopic(String[] receivedData) {
+        emailService.sendEmail(receivedData[0], "Your account status", receivedData[1]);
+        System.out.println("Received message: " + Arrays.toString(receivedData));
+    }
+}
